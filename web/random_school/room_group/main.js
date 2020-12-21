3fa97d09fb6a9d058a54aae2e48c60d1e4f753de
sunyinggang <136080416@qq.com>
2020-12-21 12:32:10 +0800

    改进

---
+++ web/random_school/room_group/main.js
@@ -0,0 +1,57 @@
+const { app, BrowserWindow,dialog } = require('electron')
+
+// 热加载
+try {
+    require('electron-reloader')(module,{});
+} catch (_) {}
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 1000,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
+
+const fs = require('fs')
+const xlsx = require('xlsx')
+const {ipcMain} = require('electron')
+
+// const root = fs.readdirSync(__dirname)
+// console.log(root)
+ipcMain.handle('ping', (evidence, ...args) => {
+    console.log('got ping', args)
+})
+
+ipcMain.handle('error', (evidence, ...args) => {
+    console.log(args)
+    dialog.showMessageBox({
+        title  : '错误'
+       , type  : 'error'
+       , message : args[0]
+      })
+})
+
+   
+
+
+
